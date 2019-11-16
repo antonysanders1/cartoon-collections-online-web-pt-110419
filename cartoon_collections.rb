a973f4b38aa65a94a0e6f70ea88d94ecb07f9fd4
Antony Sanders <antony.devon92@gmail.com>
2019-11-16 23:22:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -26,6 +26,7 @@ def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   
-  array.
+  array.find do |cheese|
+    if 
   
 end
