fa8600b4b99cce5cbf8d9d9de060caae48d8a846
Antony Sanders <antony.devon92@gmail.com>
2019-11-16 23:29:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,8 +18,11 @@ end
 def long_planeteer_calls(calls)# code an argument here
   # Your code here
   calls.detect do |long|
-    long.length < 4
-    return true
+      if long.length < 4
+      return true
+    else
+      return false 
+    end 
   end 
 end
 
